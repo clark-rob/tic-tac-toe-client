4e92b84079b818fc3523fde9f22d274ce5e12a82
clark-rob <clark.rob.g@gmail.com>
2018-10-31 15:22:59 -0400

    HTML/CSS modified, created event.js
    Formed 'gameBoard' using divs and CSS
    - created event.js, made player variables
    -began writing code to connect JS to HTML

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,8 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const authEvents = require('./auth/events.js')
 
 $(() => {
-  // your JS code goes here
+  $('gameBoard').on('click', authEvents.place) // place an X on the gameboard with a click
 })
