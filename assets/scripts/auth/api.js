a5d03c68a7a345bdd4930207cf2012fdb228ab1d
clark-rob <clark.rob.g@gmail.com>
2018-11-04 12:22:12 -0500

    Winning Path Found
    -Used jQuery to determine winner by creating '&&' statements within
    if...else statements
    -able to get winner console logged, and logged if lost
    -Need to find how to write 'tie' scenario

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -5,8 +5,6 @@
 const config = require('../config.js')
 const store = require('../store.js')
 
-
-
 const signUp = data => {
   return $.ajax({
     url: config.apiUrl + '/sign-up',
--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -43,9 +43,8 @@ let currentMove = 1 // move to begin with
 // gameBoard:
 // let game = $('#game') // tic tac board
 let gameBoard
-const boxes = document.querySelectorAll('.box') // tic-tac box
-const winningPath = [ // each box has content that equals player
-  [0, 1, 2],
+const winningPath = [$('#0').text(), $('#1').text(), $('#2').text(), $('#3').text(), $('#4').text(), $('#5').text(), $('#6').text(), $('#7').text(), $('#8').text()] // each box has content that equals player
+/*  [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
   [0, 3, 6],
@@ -53,7 +52,7 @@ const winningPath = [ // each box has content that equals player
   [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6]
-]
+] */
 // to win you need 3 values
 // const startGame = [] // begin with empty array
 // let move = 0 // number of moves
@@ -69,7 +68,7 @@ const nextPlayer = function () {
     currentPlayer = players[0].name // set current to P1
     players[0].boxes.push(event.target.id) // selected box is pushed to P1 box
     players[0].move++ // playerOne move increments 1
-    // console.log('p1 moves ' + players[0].move)
+    console.log('p1 moves ' + players[0].move)
     return currentPlayer// playerOne is set
   } else { // if current move is not a factor of 2
     currentPlayer = players[1].name // set player to P2
@@ -86,38 +85,27 @@ const nextPlayer = function () {
 // if box 0 1 2 have content that is X, X wins; if they have O, O wins, else tie
 const gameCheck = function () {
   console.log('current move= ' + currentMove)
-  console.log('Gb is ' + typeof winningPath)
-  console.log('P1 boxes are ' + typeof players[0].boxes)
-  if (players[0].moves >= 3) { // if P1 reaches 3 moves
-    for (let i = 0; i < winningPath.length; i++) {
-      if (players[0].boxes === winningPath[i]) {
-        console.log('win')
-      } else {
-        console.log('lose')
-      }
-    }
-    /* if (boxesInPlay === winningPath) { // if selected three content equal a winningPath values
-    if (boxesInPlay[0] && boxesInPlay[1] && boxesInPlay[2] === winningPath[0]) {
-      console.log('win')// check selected boxes(boxesInPlay) and compare to winningPath to determine win
-    } else if (boxesInPlay[3] === winningPath[3] && boxesInPlay[4] === winningPath[4] && boxesInPlay[5] === winningPath[5]) {
-      console.log('win')
-    } else if (boxesInPlay[6] === winningPath[6] && boxesInPlay[7] === winningPath[7] && boxesInPlay[8] === winningPath[8]) {
-      console.log('win')
-    } else if (boxesInPlay[0] === winningPath[0] && boxesInPlay[3] === winningPath[3] && boxesInPlay[6] === winningPath[6]) {
-      console.log('win')
-    } else if (boxesInPlay[1] === winningPath[1] && boxesInPlay[4] === winningPath[4] && boxesInPlay[7] === winningPath[7]) {
-      console.log('win')
-    } else if (boxesInPlay[2] === winningPath[2] && boxesInPlay[5] === winningPath[5] && boxesInPlay[8] === winningPath[8]) {
-      console.log('win')
-    } else if (boxesInPlay[0] === winningPath[0] && boxesInPlay[4] === winningPath[4] && boxesInPlay[8] === winningPath[8]) {
-      console.log('win')
-    } else if (boxesInPlay[2] === winningPath[2] && boxesInPlay[4] === winningPath[4] && boxesInPlay[6] === winningPath[6]) {
-      console.log('win')
+  console.log('div #0 = ' + $('#0').text())
+  if (players[0].move >= 3) { // if P1 reaches 3 moves
+    if ($('#0').text() === $('#1').text() && $('#1').text() === $('#2').text()) { // if the jQuery Id equal to each other you get a winner
+      console.log('Winner is ' + currentPlayer) // current player is then awarded winner
+    } else if ($('#3').text() === $('#4').text() && $('#4').text() === $('#5').text()) {
+      console.log('Winner is ' + currentPlayer)
+    } else if ($('#6').text() === $('#7').text() && $('#7').text() === $('#8').text()) {
+      console.log('Winner is ' + currentPlayer)
+    } else if ($('#0').text() === $('#3').text() && $('#3').text() === $('#6').text()) {
+      console.log('Winner is ' + currentPlayer)
+    } else if ($('#1').text() === $('#4').text() && $('#4').text() === $('#7').text()) {
+      console.log('Winner is ' + currentPlayer)
+    } else if ($('#2').text() === $('#5').text() && $('#5').text() === $('#8').text()) {
+      console.log('Winner is ' + currentPlayer)
+    } else if ($('#0').text() === $('#4').text() && $('#4').text() === $('#8').text()) {
+      console.log('Winner is ' + currentPlayer)
+    } else if ($('#2').text() === $('#4').text() && $('#4').text() === $('#6').text()) {
+      console.log('Winner is ' + currentPlayer)
     } else {
-      console.log('lose')
-    } */
-    // console.log('box in play= ' + boxesInPlay)
-    // console.log('win path= ' + winningPath[0])
+      console.log('You are not the Winner')
+    }
   }
 }
 // determine value of winner by using currentPlayer
@@ -125,33 +113,20 @@ const gameCheck = function () {
 const onBoxClick = event => {
   $(event.target).append(nextPlayer) // on the box click, player change
   // console.log(typeof event.target.id)
-  const selectedBox = $(event.target).text() // takes index of selected box and writes the content
+  // const selectedBox = $(event.target).text() // takes index of selected box and writes the content
   // console.log('GB= ' + gameBoard)
   // console.log('box content= ' + selectedBox)
-  console.log('target id= ' + event.target.id)
-  console.log('Current player= ' + currentPlayer)
-  console.log('p1 boxes= ' + players[0].boxes)
-  console.log('p2 boxes= ' + players[1].boxes)
+  console.log('target id= ' + event.target.id) // box selected
+  console.log('Current player= ' + currentPlayer) // 'M' or 'L'
+  console.log('p1 boxes= ' + players[0].boxes) // P1 array of boxes
+  console.log('p2 boxes= ' + players[1].boxes) // P2 array of boxes
+
   // boxesInPlay.push(selectedBox)// box content is pushed to boxesInPlay
   gameCheck()
-  gameBoard = $('#game').text() // makes gameBoard a variable that takes in content of the jQuery id of 'game' as a string
+  // gameBoard = $('#game').text() // makes gameBoard a variable that takes in content of the jQuery id of 'game' as a string
   // console.log('GB= ' + gameBoard)
   currentMove++ // each click, currentMove increments 1
 }
-
-/* const createGame = function () {
-  for (let i = 0; i < box.length; i++) { // for each box in the array
-
-  }
-}
-// see if current player can become playerOne - done
-// try to find who is current player - done
-// set up winning scenerio
-// if currentMove = 8 result=tie
-// continue to use onBoxClick function to find array
-$('#retry').on('click', function () {
-  event.preventDefault()
-}) */
 
 module.exports = {
   onCreateGame,
